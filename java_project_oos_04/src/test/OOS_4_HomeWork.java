7af332135d7bac1d3092328deef03bac2953e191
korea3500 <bag0437@naver.com>
2020-09-27 19:37:20 +0900

    delete file and create new java_project

---
+++ java_project_oos_04/src/test/OOS_4_HomeWork.java
@@ -0,0 +1,13 @@
+package test;
+
+public class OOS_4_HomeWork {
+	public static void main(String []args) {
+		String name;
+		int grade;
+		
+		name = "baek";
+		grade = 23;
+		
+		System.out.println("name : " + name + "\tgrade : " + grade);
+	}
+}
